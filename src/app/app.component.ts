eb5c5b2adb2512053109eb559096e2160254e265
digglue_keishi_kubo <57742279+diggluekeishikubo@users.noreply.github.com>
2023-03-25 21:46:11 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    
+  `,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'chat-gpt-sample';
+}
